a400e9e125dddee84e4bec611e2808ebc437edd1
Justin Lee <triplez@justinlee.sg>
2024-09-18 16:00:23 +0800

    Refactor free.c: Free buffer address in free_fdf function and properly free resources in free_map and free_fdf functions

--- srcs/utils/free.c
+++ srcs/utils/free.c
@@ -6,7 +6,7 @@
 /*   By: welee <welee@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/15 16:14:56 by welee             #+#    #+#             */
-/*   Updated: 2024/09/18 15:46:32 by welee            ###   ########.fr       */
+/*   Updated: 2024/09/18 16:00:07 by welee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,6 +48,7 @@ void	free_map(t_map *map)
 	}
 	free(map->vertices);
 	map->vertices = NULL;
+	free(map);
 }
 
 /**
@@ -63,11 +64,15 @@ void	free_fdf(t_fdf *fdf)
 	if (fdf->buffer.img)
 		mlx_destroy_image(fdf->mlx_ptr, fdf->buffer.img);
 	if (fdf->win_ptr)
+	{
 		mlx_destroy_window(fdf->mlx_ptr, fdf->win_ptr);
+		fdf->win_ptr = NULL;
+	}
 	if (fdf->mlx_ptr)
 	{
 		mlx_loop_end(fdf->mlx_ptr);
 		mlx_destroy_display(fdf->mlx_ptr);
 		free(fdf->mlx_ptr);
+		fdf->mlx_ptr = NULL;
 	}
 }
