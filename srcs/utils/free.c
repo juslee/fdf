ca575042f38c24cfc7335db37bcd031e3f31abee
Justin Lee <triplez@justinlee.sg>
2024-09-19 12:31:40 +0800

    Refactor free.c: Fix memory leak in free_fdf function

--- srcs/utils/free.c
+++ srcs/utils/free.c
@@ -6,7 +6,7 @@
 /*   By: welee <welee@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/15 16:14:56 by welee             #+#    #+#             */
-/*   Updated: 2024/09/19 12:30:57 by welee            ###   ########.fr       */
+/*   Updated: 2024/09/19 12:31:36 by welee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,7 @@ void	free_map(t_map *map)
  */
 void	free_fdf(t_fdf *fdf)
 {
-	if (fdf->map
+	if (fdf->map)
 		free_map(fdf->map);
 	if (fdf->img_ptr)
 		mlx_destroy_image(fdf->mlx_ptr, fdf->img_ptr);
